b9a6486834eaec222011f78a1c76f47af489d52e
Lizzie LaShark <email": null>
2016-05-16 16:09:30 +1200

    all finished

--- my-slush-pages/index.js
+++ my-slush-pages/index.js
@@ -6,33 +6,32 @@ class CountAndGreet extends React.Component {
   constructor(props) {
     super(props)
     this.state ={counter: 0}
-    this.state ={Catcounter: 0}
 
   }
   clicked() {
     console.log('got clicked')
     this.setState({counter: this.state.counter + 1})
-    this.setState({Catcounter: this.state.Catcounter + 1})
   }
 
   render() {
     return (
-      <main>
         <div onClick={this.clicked.bind(this)}>
-          <h1>Kia ora {this.props.HumanName}</h1>
+          <h1>Kia ora {this.props.name}</h1>
           <h2>count is on {this.state.counter}</h2>
         </div>
-        <div onClick={this.clicked.bind(this)}>
-          <h1>Kia ora {this.props.CatName}</h1>
-          <h2>count is on {this.state.Catcounter}</h2>
-        </div>
-    </main>
     )
   }
 }
 
-render(<CountAndGreet HumanName="Lizzie" counter="0" CatName="Nancy" Catcounter="0"/>,
-document.querySelector('main'))
-console.log('welcome to play-with-react')
+render((
+  <div>
+    <CountAndGreet name="Lizzie" />
+    <CountAndGreet name="Piet" />
+  </div>
+    ),
+    document.querySelector('main'))
 
-//spend some more time on this trying to get the counters working separately.
+
+
+
+console.log('welcome to play-with-react')
